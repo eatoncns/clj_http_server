806c2e37da5447ee77216de5c80d8309257020cf
Philip Meier <philip.meier06@googlemail.com>
2017-10-03 15:11:13 +0100

    Add basic request parsing

--- src/http_server/request.clj
+++ src/http_server/request.clj
@@ -1,9 +1,12 @@
-(ns http-server.request)
+(ns http-server.request
+  (:require [clojure.string :as str]))
 
 (defrecord Request [method uri])
 
 (defn parse [input]
-  (map->Request {:method "GET" :uri "/foo"}))
+  (let [[request-line _] (str/split input #"\r\n" 2)
+        [method uri version] (str/split request-line #" " 3)]
+    (map->Request {:method method :uri uri})))
 
 (defn process [request]
   "HTTP/1.1 200 OK\r\n\r\nHello World")
