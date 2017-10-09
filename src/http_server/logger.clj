d9450a81fb010cfd94f1edb008752171b93b28b2
Philip Meier <philip.meier06@googlemail.com>
2017-10-09 10:31:16 +0100

    Write logs to file

--- src/http_server/logger.clj
+++ src/http_server/logger.clj
@@ -1,5 +1,11 @@
 (ns http-server.logger
-  (:require [taoensso.timbre :refer [info]]))
+  (:require [clojure.java.io :as io]
+            [taoensso.timbre :as timbre :refer [info]]
+            [taoensso.timbre.appenders.core :as appenders]))
+
+(def log-file-name "/tmp/server-log.txt")
+(io/delete-file log-file-name :quiet)
+(timbre/merge-config! {:appenders {:spit (appenders/spit-appender {:fname log-file-name})}})
 
 (defn log [msg]
   (info msg)
