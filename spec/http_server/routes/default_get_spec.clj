708d5ebefba3eb2b82a7c438947bf4ef2587d0bc
Philip Meier <philip.meier06@googlemail.com>
2017-10-10 15:41:02 +0100

    Remove some duplication in the default get tests

--- spec/http_server/routes/default_get_spec.clj
+++ spec/http_server/routes/default_get_spec.clj
@@ -11,41 +11,39 @@
   (file-exists? [this path] (= path "/foo.png"))
   (file-data [this path] "blah"))
 
+(defn run-get [path]
+  (->> (FakeFileInfo.)
+       (process-get path)))
+
 (describe "process-get"
 
   (it "returns 200 when path is a valid directory"
-    (->> (FakeFileInfo.)
-         (process-get "/dir")
+    (->> (run-get "/dir")
          (:status)
          (should= 200)))
 
   (it "returns file listing page as body for valid directory"
-    (->> (FakeFileInfo.)
-         (process-get "/dir")
+    (->> (run-get "/dir")
          (:body)
          (should-contain "hello.txt")))
 
   (it "returns 404 when path does not exist"
-    (->> (FakeFileInfo.)
-         (process-get "/foobar")
+    (->> (run-get "/foobar")
          (:status)
          (should= 404)))
 
   (it "returns 200 when path is a file that exists"
-    (->> (FakeFileInfo.)
-         (process-get "/foo.png")
+    (->> (run-get "/foo.png")
          (:status)
          (should= 200)))
 
   (it "returns file contents as body when path is a file that exists"
-    (->> (FakeFileInfo.)
-         (process-get "/foo.png")
+    (->> (run-get "/foo.png")
          (:body)
          (should= "blah")))
 
   (it "returns content-type header based on file type"
-    (->> (FakeFileInfo.)
-         (process-get "/foo.png")
+    (->> (run-get "/foo.png")
          ((func/flip get-in) [:headers "Content-Type"])
          (should= "image/png")))
 )
