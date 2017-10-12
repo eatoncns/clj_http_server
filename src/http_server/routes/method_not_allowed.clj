b1eec522be21218c2a0d459099a1ef227dffd409
Philip Meier <philip.meier06@googlemail.com>
2017-10-12 10:46:15 +0100

    Add method not allowed route

---
+++ src/http_server/routes/method_not_allowed.clj
@@ -0,0 +1,31 @@
+(ns http-server.routes.method-not-allowed
+  (:require [http-server.routes.route :as route]
+            [http-server.response :refer [map->Response]]
+            [clojure.string :as string])
+  (:import [http_server.response Response]))
+
+(defn- append-method [current method]
+  (str current method ", "))
+
+(defn- remove-trailing-comma [input]
+  (apply str (drop-last input)))
+
+(defn- build-header [allow-string]
+  {"Allow" allow-string})
+
+(defn- allowed-header [allowed]
+  (->> allowed
+       (reduce append-method "")
+       (string/trim)
+       (remove-trailing-comma)
+       (build-header)))
+
+(defrecord MethodNotAllowed [request allowed]
+  route/Route
+  (is-applicable [this]
+    true)
+
+  (process [this directory-served]
+    (map->Response {:status 405 :headers (allowed-header allowed) :body nil}))
+)
+
