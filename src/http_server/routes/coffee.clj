a69a226e64767a5a81d6993ed27d1bf7db90e663
Philip Meier <philip.meier06@googlemail.com>
2017-10-12 21:35:32 +0100

    Cleanup coffee route

--- src/http_server/routes/coffee.clj
+++ src/http_server/routes/coffee.clj
@@ -1,13 +1,13 @@
 (ns http-server.routes.coffee
   (:require [http-server.routes.route :as route]
-            [http-server.response :refer [map->Response]])
-  (:import [http_server.response Response]))
+            [http-server.response :refer [map->Response]]
+            [http-server.constants.methods :refer [GET]]))
 
 (defrecord Coffee [request]
   route/Route
   (is-applicable? [this directory-served]
     (and (= (get-in this [:request :uri]) "/coffee")
-         (= (get-in this [:request :method]) "GET")))
+         (= (get-in this [:request :method]) GET)))
 
   (process [this directory-served]
     (map->Response {:status 418 :headers {} :body "I'm a teapot"}))
