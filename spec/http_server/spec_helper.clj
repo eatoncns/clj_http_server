b730103f06e737417082f3082fac19e089d59514
Philip Meier <philip.meier06@googlemail.com>
2017-10-12 14:35:52 +0100

    Remove duplication in coffee spec

--- spec/http_server/spec_helper.clj
+++ spec/http_server/spec_helper.clj
@@ -1,8 +1,20 @@
 (ns http-server.spec-helper
-  (:require [speclj.core :refer [should= should-be-a]]))
+  (:require [speclj.core :refer [should= should-be-a]]
+            [http-server.routes.route :as route]))
 
 (defn rshould= [actual expected]
   (should= expected actual))
 
 (defn rshould-be-a [actual expected]
   (should-be-a expected actual))
+
+(defn applicable-should= [route-instance expected]
+  (-> route-instance
+      (route/is-applicable? "directory-served")
+      (rshould= expected)))
+
+(defn response-should-have [route-instance field expected]
+  (-> route-instance
+      (route/process "directory served")
+      (get field)
+      (rshould= expected)))
