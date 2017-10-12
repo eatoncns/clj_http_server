97431de722f307c1927503908b6163f11c20e939
Philip Meier <philip.meier06@googlemail.com>
2017-10-12 15:56:26 +0100

    Remove some duplication in method options spec

--- spec/http_server/routes/method_options_spec.clj
+++ spec/http_server/routes/method_options_spec.clj
@@ -1,33 +1,32 @@
 (ns http-server.routes.method-options-spec
   (:require [speclj.core :refer :all]
             [http-server.routes.method-options :refer :all]
-            [http-server.routes.route :as route]))
+            [http-server.routes.route :as route]
+            [http-server.spec-helper :refer :all]
+            [http-server.constants.methods :refer :all]))
+
+(def applicable-methods #{POST HEAD PUT GET OPTIONS})
+
+(defn method-options-request [method uri]
+  (map->MethodOptions {:request {:method method :uri uri}}))
 
 (describe "is-applicable?"
-  (it "returns true for any method to /method_options"
-    (doseq [method ["POST" "HEAD" "PUT" "GET" "OPTIONS"]]
-      (-> (map->MethodOptions{:request {:method method :uri "/method_options"}})
-          (route/is-applicable? "directory-served")
-          (should= true))))
+  (it "returns true for applicale method to /method_options"
+    (doseq [method applicable-methods]
+      (applicable-should= (method-options-request method "/method_options") true)))
 
   (it "returns false for any method to other uri"
-    (doseq [method ["POST" "HEAD" "PUT" "GET" "OPTIONS"]]
-      (-> (map->MethodOptions{:request {:method method :uri "/met"}})
-          (route/is-applicable? "directory-served")
-          (should= false))))
+    (doseq [method applicable-methods]
+      (applicable-should= (method-options-request method "/met") false)))
 )
 
 (describe "process"
-  (it "returns 200 status for all methods"
-    (doseq [method ["POST" "HEAD" "PUT" "GET" "OPTIONS"]]
-      (-> (map->MethodOptions{:request {:method method :uri "/method_options"}})
-          (route/process "directory-served")
-          (get :status)
-          (should= 200))))
+  (it "returns 200 status for applicable methods"
+    (doseq [method applicable-methods]
+      (-> (method-options-request method "/method_options")
+          (response-should-have :status 200))))
 
   (it "returns header allowing all methods for OPTION"
-    (-> (map->MethodOptions{:request {:method "OPTION" :uri "/method_options"}})
-        (route/process "directory-served")
-        (get-in [:headers "Allow"])
-        (should= "GET,HEAD,POST,OPTIONS,PUT")))
+    (-> (method-options-request OPTIONS "/method_options")
+        (response-should-have-header "Allow" "GET,HEAD,POST,OPTIONS,PUT")))
 )
