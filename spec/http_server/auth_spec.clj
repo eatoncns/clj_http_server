10eb9370669641a55592e555e5b14d48aea16260
Philip Meier <philip.meier06@googlemail.com>
2017-10-12 21:32:25 +0100

    Remove some duplication in specs

--- spec/http_server/auth_spec.clj
+++ spec/http_server/auth_spec.clj
@@ -2,49 +2,46 @@
   (:require [speclj.core :refer :all]
             [http-server.auth :refer :all]
             [http-server.request :refer [map->Request]]
-            [http-server.utils.base64 :as b64]))
+            [http-server.utils.base64 :as b64]
+            [http-server.spec-helper :refer [rshould=]]))
 
 (def auth-config {"/logs" {:username "admin" :password "hunter2"}})
 (def matching-credentials (b64/encode-string "admin:hunter2"))
+
+(defn authorised-should= [request expected]
+  (-> request
+      (authorise auth-config)
+      (get :authorised)
+      (rshould= expected)))
 
 (describe authorise
 
   (it "sets authorised true when uri not in config"
     (-> (map->Request{:method "GET" :uri "/foo" :version "HTTP/1.1" :headers {}})
-        (authorise auth-config)
-        (get :authorised)
-        (should= true)))
+        (authorised-should= true)))
 
   (it "sets authorised false for matching uri with no header"
     (-> (map->Request{:method "GET" :uri "/logs" :version "HTTP/1.1" :headers {}})
-        (authorise auth-config)
-        (get :authorised)
-        (should= false)))
+        (authorised-should= false)))
 
   (it "sets authorised true for matching uri with correct header"
     (-> (map->Request{:method "GET"
                       :uri "/logs"
                       :version "HTTP/1.1"
                       :headers {"Authorization" (str "Basic " matching-credentials)}})
-        (authorise auth-config)
-        (get :authorised)
-        (should= true)))
+        (authorised-should= true)))
 
   (it "sets authorised false for matching uri with invalid syntax header"
     (-> (map->Request{:method "GET"
                       :uri "/logs"
                       :version "HTTP/1.1"
                       :headers {"Authorization" (str "Bas" matching-credentials)}})
-        (authorise auth-config)
-        (get :authorised)
-        (should= false)))
+        (authorised-should= false)))
 
   (it "sets authorised false for matching uri with incorrect credentials"
     (-> (map->Request{:method "GET"
                       :uri "/logs"
                       :version "HTTP/1.1"
                       :headers {"Authorization" (str "Basic " (b64/encode-string "admin:hunter3"))}})
-        (authorise auth-config)
-        (get :authorised)
-        (should= false)))
+        (authorised-should= false)))
 )
