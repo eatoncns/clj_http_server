c21e9feae89646df2e5282eecbd341b7d26c067e
Philip Meier <philip.meier06@googlemail.com>
2017-10-13 11:08:03 +0100

    Add content type headers

--- src/http_server/routes/cookie.clj
+++ src/http_server/routes/cookie.clj
@@ -1,7 +1,8 @@
 (ns http-server.routes.cookie
   (:require [http-server.routes.route :as route]
             [http-server.response :refer [map->Response]]
-            [http-server.constants.methods :refer [GET]]))
+            [http-server.constants.methods :refer [GET]]
+            [http-server.constants.content-type :refer [content-type]]))
 
 
 (defn- build-cookie [m k v]
@@ -15,6 +16,8 @@
 
   (process [this directory-served]
     (let [cookie (reduce-kv build-cookie "" (get-in this [:request :params]))]
-      (map->Response {:status 200 :headers {"Set-Cookie" cookie} :body "Eat"})))
+      (map->Response {:status 200
+                      :headers (merge {"Set-Cookie" cookie} (content-type "text"))
+                      :body "Eat"})))
 )
 
