c459f3c376ebf911a4ece668cd0fb526399bbf69
Philip Meier <philip.meier06@googlemail.com>
2017-10-13 12:55:43 +0100

    Add Content-Range header to partial file response

--- src/http_server/routes/file.clj
+++ src/http_server/routes/file.clj
@@ -5,7 +5,8 @@
             [http-server.utils.file-info :as fi]
             [http-server.utils.range :as rg]
             [http-server.utils.functional :refer [in?]]
-            [http-server.constants.methods :refer :all])
+            [http-server.constants.methods :refer :all]
+            [clojure.string :as string])
   (:import [http_server.utils.file_info FileInfoAtRoot]))
 
 (defn is-request-applicable [request file-info]
@@ -16,17 +17,30 @@
   (let [headers (get request :headers)]
     (contains? headers "Range")))
 
-(defn partial-file-data [file-info path request]
-  (let [range-string (get-in request [:headers "Range"])
+(defn request-range [request]
+  (get-in request [:headers "Range"]))
+
+(defn partial-file-data [request file-info path]
+  (let [range-string (request-range request)
         [start length] (rg/calculate-range file-info path range-string)]
     (fi/partial-file-data file-info path start length)))
+
+(defn content-range-header [request file-info path]
+  (let [range-string (string/replace (request-range request) #"=" " ")
+        file-length (str (fi/file-length file-info path))
+        content-range (str range-string "/" file-length)]
+    {"Content-Range" content-range}))
+
+(defn partial-headers [request file-info path]
+  (merge (content-type (fi/extension path))
+         (content-range-header request file-info path)))
 
 (defn process-request [request file-info]
   (let [path (get request :uri)]
     (if (requests-range? request)
       (map->Response {:status 206
-                      :headers (content-type (fi/extension path))
-                      :body (partial-file-data file-info path request)})
+                      :headers (partial-headers request file-info path)
+                      :body (partial-file-data request file-info path)})
       (map->Response {:status 200
                       :headers (content-type (fi/extension path))
                       :body (fi/file-data file-info path)}))))
